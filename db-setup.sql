989f90f473f8113588553fb35ed76c9fdb965167
ql65 <ql65@cornell.edu>
2017-05-12 13:43:14 -0400

    Update db-setup.sql

--- db-setup.sql
+++ db-setup.sql
@@ -7,11 +7,11 @@
  * This file contains SQL statements necessary to set up a new database
  * supporting stack map querying and storage.
  */
- 
+
  create database LibraryStacks;
 
  create table Library(
-     lid INT(5) NOT NULL AUTO_INCREMENT, 
+     lid INT(5) NOT NULL AUTO_INCREMENT,
      name CHAR(25) PRIMARY KEY
  );
 
@@ -42,10 +42,10 @@
      center_y INT(9),
      length INT(9),
      width INT(9),
-     rotation INT(9), 
+     rotation INT(9),
      FOREIGN KEY(floor)
        REFERENCES Floor(name)
-       ON UPDATE CASCADE ON DELETE RESTRICT   
+       ON UPDATE CASCADE ON DELETE RESTRICT
  );
 
  /* Aisle is for book stacks that hold books */
@@ -58,7 +58,7 @@
      rotation INT(9),
      sides INT(1),
      category CHAR(10),
-     call_range CHAR(1000)
+     call_range CHAR(1000),
      FOREIGN KEY(aislearea)
        REFERENCES AisleArea(aaid)
        ON UPDATE CASCADE ON DELETE RESTRICT
@@ -75,4 +75,11 @@
      FOREIGN KEY(floor)
        REFERENCES Floor(name)
        ON UPDATE CASCADE ON DELETE RESTRICT
+ );
+
+ /* Create token table*/
+ create table Token(
+     tid INT(9) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+     token CHAR(25),
+     expiration DATETIME(20),
  );
