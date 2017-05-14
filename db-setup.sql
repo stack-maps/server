7b2fa973256250c2811b87f07c7b7bd84dfe6791
ql65 <ql65@cornell.edu>
2017-05-13 21:30:23 -0400

    Update db-setup.sql

--- db-setup.sql
+++ db-setup.sql
@@ -8,78 +8,122 @@
  * supporting stack map querying and storage.
  */
 
- create database LibraryStacks;
+ /*create database LibraryStacks;*/
 
- create table Library(
-     lid INT(5) NOT NULL AUTO_INCREMENT,
-     name CHAR(25) PRIMARY KEY
- );
+ create table Library (
+    lid INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    lname CHAR(25) NOT NULL
+ )  ENGINE=INNODB;
 
- create table Floor(
-     fid INT(9) NOT NULL AUTO_INCREMENT,
-     name CHAR(25),
-     FOREIGN KEY(library)
-       REFERENCES Library(name)
-       ON UPDATE CASCADE ON DELETE RESTRICT,
-    PRIMARY KEY(name, library)
- );
+ create table Floor (
+     fid INT NOT NULL AUTO_INCREMENT,
+     fname CHAR(25) NOT NULL,
+     forder FLOAT,
+     library INT NOT NULL,
+
+     PRIMARY KEY(fid),
+     INDEX (library),
+
+     FOREIGN KEY (library)
+       REFERENCES Library(lid)
+       ON UPDATE CASCADE ON DELETE RESTRICT
+ )   ENGINE=INNODB;
 
  create table Wall(
-     wid INT(9) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-     x1 INT(9),
-     y1 INT(9),
-     x2 INT(9),
-     y2 INT(9)
-     FOREIGN KEY(floor)
-       REFERENCES Floor(name)
+     wid INT NOT NULL AUTO_INCREMENT,
+     x1 FLOAT,
+     y1 FLOAT,
+     x2 FLOAT,
+     y2 FLOAT,
+     floor INT,
+
+     PRIMARY KEY(wid),
+     INDEX(floor),
+     FOREIGN KEY (floor)
+       REFERENCES Floor(fid)
        ON UPDATE CASCADE ON DELETE RESTRICT
- );
+ )   ENGINE=INNODB;
 
  /* AisleArea is an area that has aisles */
  create table AisleArea(
      aaid INT(9) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-     center_x INT(9),
-     center_y INT(9),
-     length INT(9),
-     width INT(9),
-     rotation INT(9),
+     center_x FLOAT(9),
+     center_y FLOAT(9),
+     length FLOAT(9),
+     width FLOAT(9),
+     rotation FLOAT(9),
+     floor INT,
+
+     INDEX(floor),
      FOREIGN KEY(floor)
-       REFERENCES Floor(name)
+       REFERENCES Floor(fid)
        ON UPDATE CASCADE ON DELETE RESTRICT
- );
+ )   ENGINE=INNODB;
 
  /* Aisle is for book stacks that hold books */
  create table Aisle(
      aid INT(9) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-     center_x INT(9),
-     center_y INT(9),
-     length INT(9),
-     width INT(9),
-     rotation INT(9),
+     center_x FLOAT(9),
+     center_y FLOAT(9),
+     length FLOAT(9),
+     width FLOAT(9),
+     rotation FLOAT(9),
      sides INT(1),
      category CHAR(10),
-     call_range CHAR(1000),
+     aislearea INT,
+     floor INT,
+
+     INDEX(aislearea),
      FOREIGN KEY(aislearea)
        REFERENCES AisleArea(aaid)
-       ON UPDATE CASCADE ON DELETE RESTRICT
+       ON UPDATE CASCADE ON DELETE CASCADE,
+     INDEX(floor),
+       FOREIGN KEY(floor)
+         REFERENCES Floor(fid)
+         ON UPDATE CASCADE ON DELETE RESTRICT
  );
+
+/* Call_Range is used to save the call ranges for each aisle*/
+create table Call_Range(
+    cid INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    collection CHAR(50),
+    callstart CHAR(50),
+    callend CHAR(50),
+    side INT,
+    aisle INT(9),
+
+    INDEX(aisle),
+    FOREIGN KEY(aisle)
+      REFERENCES Aisle(aid)
+      ON UPDATE CASCADE ON DELETE CASCADE
+);
 
  /* Landmark is for icons other than book stacks, such as elevators, stairs, etc. */
  create table Landmark(
      lmid INT(9) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-     name CHAR(25),
-     center_x INT(9),
-     center_y INT(9),
-     length INT(9),
-     width INT(9),
+     lname CHAR(25),
+     center_x FLOAT(9),
+     center_y FLOAT(9),
+     rotation FLOAT(9),
+     length FLOAT(9),
+     width FLOAT(9),
+     floor INT,
+
+     INDEX(floor),
      FOREIGN KEY(floor)
-       REFERENCES Floor(name)
+       REFERENCES Floor(fid)
        ON UPDATE CASCADE ON DELETE RESTRICT
  );
 
  /* Create token table*/
  create table Token(
      tid INT(9) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-     token CHAR(25),
-     expiration DATETIME(20),
+     token CHAR(32),
+     expiration DATETIME
+ );
+
+ /* Create table to save username and password*/
+ create table Users(
+     username CHAR(25),
+     password CHAR(25)
  );
