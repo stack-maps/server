ec63a3eaf7f07798f7fcab4cc15e7fc77958393f
Jiacong Xu <jiacong.xu@foxmail.com>
2017-05-18 22:50:21 -0400

    Modified db-setup.

--- db-setup.sql
+++ db-setup.sql
@@ -31,7 +31,7 @@ CREATE TABLE floor (
     PRIMARY KEY (floor_id),
     floor_id        INT         NOT NULL    AUTO_INCREMENT,
     floor_name      CHAR(25),
-    floor_order     FLOAT,
+    floor_order     FLOAT       NOT NULL    DEFAULT 0,
     library         INT         NOT NULL,
                     INDEX (library),
                     FOREIGN KEY (library) REFERENCES library(library_id)
@@ -41,11 +41,11 @@ CREATE TABLE floor (
 CREATE TABLE wall (
     PRIMARY KEY (wall_id),
     wall_id         INT         NOT NULL    AUTO_INCREMENT,
-    start_x         FLOAT,
-    start_y         FLOAT,
-    end_x           FLOAT,
-    end_y           FLOAT,
-    floor           INT,
+    start_x         FLOAT       NOT NULL    DEFAULT 0,
+    start_y         FLOAT       NOT NULL    DEFAULT 0,
+    end_x           FLOAT       NOT NULL    DEFAULT 0,
+    end_y           FLOAT       NOT NULL    DEFAULT 0,
+    floor           INT         NOT NULL,
                     INDEX(floor),
                     FOREIGN KEY (floor) REFERENCES floor(floor_id)
                     ON UPDATE CASCADE ON DELETE CASCADE
@@ -54,11 +54,11 @@ CREATE TABLE wall (
 CREATE TABLE aisle_area (
     PRIMARY KEY (aisle_area_id),
     aisle_area_id   INT         NOT NULL    AUTO_INCREMENT,
-    center_x        FLOAT,
-    center_y        FLOAT,
-    width           FLOAT,
-    height          FLOAT,
-    rotation        FLOAT,
+    center_x        FLOAT       NOT NULL    DEFAULT 0,
+    center_y        FLOAT       NOT NULL    DEFAULT 0,
+    width           FLOAT       NOT NULL    DEFAULT 0,
+    height          FLOAT       NOT NULL    DEFAULT 0,
+    rotation        FLOAT       NOT NULL    DEFAULT 0,
     floor           INT,
                     INDEX(floor),
                     FOREIGN KEY (floor) REFERENCES floor(floor_id)
@@ -68,13 +68,13 @@ CREATE TABLE aisle_area (
 CREATE TABLE aisle (
     PRIMARY KEY (aisle_id),
     aisle_id        INT         NOT NULL    AUTO_INCREMENT,
-    center_x        FLOAT,
-    center_y        FLOAT,
-    width           FLOAT,
-    height          FLOAT,
-    rotation        FLOAT,
+    center_x        FLOAT       NOT NULL    DEFAULT 0,
+    center_y        FLOAT       NOT NULL    DEFAULT 0,
+    width           FLOAT       NOT NULL    DEFAULT 0,
+    height          FLOAT       NOT NULL    DEFAULT 0,
+    rotation        FLOAT       NOT NULL    DEFAULT 0,
     is_double_sided INT(1)      NOT NULL,
-    aisle_area      INT,
+    aisle_area      INT         NOT NULL,
                     INDEX (aisle_area),
                     FOREIGN KEY (aisle_area) REFERENCES aisle_area(aisle_area_id)
                     ON UPDATE CASCADE ON DELETE CASCADE,
@@ -87,11 +87,11 @@ CREATE TABLE aisle (
 CREATE TABLE call_range (
     PRIMARY KEY (call_range_id),
     call_range_id   INT         NOT NULL    AUTO_INCREMENT,
-    collection      CHAR(50),
-    call_start      CHAR(50),
-    call_end        CHAR(50),
+    collection      CHAR(50)    NOT NULL    DEFAULT '',
+    call_start      CHAR(50)    NOT NULL    DEFAULT '',
+    call_end        CHAR(50)    NOT NULL    DEFAULT '',
     side            INT,
-    aisle           INT,
+    aisle           INT         NOT NULL,
                     INDEX(aisle),
                     FOREIGN KEY (aisle) REFERENCES aisle(aisle_id)
                     ON UPDATE CASCADE ON DELETE CASCADE
@@ -100,13 +100,13 @@ CREATE TABLE call_range (
 CREATE TABLE landmark (
     PRIMARY KEY (landmark_id),
     landmark_id     INT         NOT NULL    AUTO_INCREMENT,
-    landmark_type   CHAR(25),
-    center_x        FLOAT,
-    center_y        FLOAT,
-    width           FLOAT,
-    height          FLOAT,
-    rotation        FLOAT,
-    floor           INT,
+    landmark_type   CHAR(25)    NOT NULL    DEFAULT '',
+    center_x        FLOAT       NOT NULL    DEFAULT 0,
+    center_y        FLOAT       NOT NULL    DEFAULT 0,
+    width           FLOAT       NOT NULL    DEFAULT 0,
+    height          FLOAT       NOT NULL    DEFAULT 0,
+    rotation        FLOAT       NOT NULL    DEFAULT 0,
+    floor           INT         NOT NULL,
                     INDEX(floor),
                     FOREIGN KEY(floor) REFERENCES floor(floor_id)
                     ON UPDATE CASCADE ON DELETE RESTRICT
